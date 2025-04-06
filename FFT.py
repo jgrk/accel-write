196fed3dd4fedf50f0f8d2ef0c96bbd7a5eb6a12
Erik Duvander <e.o.duvander@gmail.com>
2025-04-06 13:59:02 +0200

    more utils

--- FFT.py
+++ FFT.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pathlib
 from readData import convert_dat_to_csv
+from matplotlib import pyplot as plt
 import pyfftw
 
 
@@ -9,6 +10,7 @@ import pyfftw
 sampling_rate = 800  # Hz
 data_dir = "data/"
 save_dir = "fft/"
+dt = 1/sampling_rate
 
 
 
@@ -28,16 +30,38 @@ def load_csv(file_name: str):
 def main():
     for file in pathlib.Path(data_dir).glob("*.dat"):
         # convert data
+
         dat_filename = f"{data_dir}{file.name}"
         convert_dat_to_csv(dat_filename)
         csv_filename = f"{data_dir}{file.name.replace('dat', 'csv')}"
         # Load CSV file
         data = np.loadtxt(csv_filename, delimiter=',', skiprows=1)
-        freqs = np.fft.fftfreq(len(data), d=1/sampling_rate)
+        N = len(data)
+        freqs = np.fft.fftfreq(N, d=dt)[:len(data) // 2]
+        fft_x = np.fft.fft(data[:,0])
+        fft_y = np.fft.fft(data[:,1])
+        fft_z = np.fft.fft(data[:,2])
+        mag_x = 20 * np.log10( np.abs(fft_x) / np.max(np.abs(fft_x)) + 1e-12 )[:len(data) // 2]
+        mag_y = 20 * np.log10( np.abs(fft_y) / np.max(np.abs(fft_y)) + 1e-12 )[:len(data) // 2]
+        mag_z = 20 * np.log10( np.abs(fft_z) / np.max(np.abs(fft_z)) + 1e-12 )[:len(data) // 2]
 
+        #fft_x_ms2 = np.fft.rfft(data[:,3])
+        #fft_x_ms2 = np.fft.rfft(data[:,4])
+        #fft_x_ms2 = np.fft.rfft(data[:,5])
 
+        for vals in [(mag_x, 'x'), (mag_y, 'y'), (mag_z, 'z')]:
+            plt.figure(figsize=(10, 6))
+            plt.plot(freqs, vals[0], label=f"{vals[1]}-axis (g)")
 
-        # Perform FFT on csv
+            # Configure plot labels and legend
+            plt.xlabel("Frequency (Hz)")
+            plt.ylabel("Magnitude (dB)")
+            plt.title("Accelerometer Data Frequency domain")
+            plt.legend()
+            plt.grid(True)
+
+            plt.savefig(f"{save_dir}{vals[1]}/{file.name.replace('dat', 'png')}")
+            # Perform FFT on csv
 
 
 if __name__ == "__main__":
