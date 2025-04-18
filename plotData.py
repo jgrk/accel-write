64cdd27dd5652d4b37a31d61c5f566e1b86a8c98
Erik Duvander <e.o.duvander@gmail.com>
2025-04-19 01:58:29 +0200

    noice

--- plotData.py
+++ plotData.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
+import scipy.signal as signal
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
-def plot_accel_data(csv_filename):
+def plot_accel_data(csv_filename, filter=False):
     """Read accelerometer data from CSV and plot X, Y, Z values."""
 
     # Load CSV file into a DataFrame
@@ -18,12 +23,19 @@ def plot_accel_data(csv_filename):
 
     # Check if required columns exist
     if not {"X (g)", "Y (g)", "Z (g)"}.issubset(accel_data.columns):
-        print(f"Error: CSV file is missing expected columns. Found: {list(accel_data.columns)}")
+        print(
+            f"Error: CSV file is missing expected columns. Found: {list(accel_data.columns)}"
+        )
         return
+
+    if filter:
+        for col in accel_data.columns:
+            logger.info(f"Filtering '{col}'")
+            accel_data[col] = signal.savgol_filter(accel_data[col], 10, 3)
 
     # Plot the accelerometer data
     plt.figure(figsize=(10, 6))
-    plt.plot(accel_data.index, accel_data["X (g)"].rolling(20).mean(), label="X-axis (g)")
+    plt.plot(accel_data.index, accel_data["X (g)"], label="X-axis (g)")
     plt.plot(accel_data.index, accel_data["Y (g)"], label="Y-axis (g)")
     plt.plot(accel_data.index, accel_data["Z (g)"], label="Z-axis (g)")
 
@@ -39,8 +51,13 @@ def plot_accel_data(csv_filename):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Plot accelerometer data from a CSV file.")
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Starting plotting.")
+    parser = argparse.ArgumentParser(
+        description="Plot accelerometer data from a CSV file."
+    )
     parser.add_argument("csv_filename", type=str, help="Path to the CSV file to plot")
+    parser.add_argument("--filter", action="store_true", help="Filter data")
     args = parser.parse_args()
 
-    plot_accel_data(args.csv_filename)
+    plot_accel_data(args.csv_filename, args.filter)
---
+++ test.py
@@ -0,0 +1,6 @@
+from ski_analys import enhanced_fft
+import pytest
+
+
+def test_enhanced_fft():
+    pass
